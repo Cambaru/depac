0e21bcfb0eba17af834a795c3a1f0729b83581f3
Cambaru <luispreuss@t-online.de>
2018-02-05 20:04:28 +0100

    Code bereinigt und umgeschrieben

--- server.js
+++ server.js
@@ -2,157 +2,202 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
-/*app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});*/
-app.use(require('express').static(path.join(__dirname, 'public')));
+
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/public/index.html');
+});
+//app.use(require('express').static(path.join(__dirname, 'public')));
 var d = new Date();
 
-//Bereits gesendete Nachrichten
-sentmessages = [];
 
+sentmessages = [];
 img = [];
 
 
-//Liste aller Admins
-operators = [];
 
-// 2d Array [socketID],[username]
-names = [];
+//Einzelner Benutzer
+class User{
+    constructor(sid){
+        this.id = sid;
+        this.name = "";
+        this.isAdmin = false;
+        this.canPing = false;
+    }
 
-//Speichert benutzer die Pingen dürfen
-ping = [];
+    MakeAdmin(){
+        this.isAdmin = true;
+        canPing = true;
+    }
+    UnmakeAdmin(){
+        this.isAdmin = false;
+        canPing = false;
+    }
+    EnablePing(){
+        this.canPing = true;
+    }
+    DisablePing(){
+        this.canPing = false;
+    }
+}
+
+user = [];
+//Alle Benutzer
+class Users{
+    GetIndexByName(name){
+        var i=0;
+        user.forEach(element => {
+            i++;
+            if(element.name == name){
+                return i;
+            }
+        });
+        return -1;
+    }
+    GetIndexById(id){
+        for(var i = 0; i< user.length;i++){
+            if(user[i].id == id){
+                return i;
+            }
+        }
+        return -1;
+    }
+    UserHasName(id){
+        for(var i = 0; i< user.length;i++){
+            if(user[i].id == id){
+                if(user[i].name != ""){
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
+    GetUserIdByName(name){
+        user.forEach(u => {
+            if(u.name == name){
+                return u.id;
+            }
+        });
+    }
+    GetUserNameById(id){
+        for(var i = 0; i< user.length;i++){
+            if(user[i].id == id){
+                return user[i].name;
+            }
+        }
+        return false;
+    }
+    IsNameTaken(name){
+        user.forEach(u => {
+            if(u.name == name){
+                return true;
+            }
+        });
+        return false;
+    }
+
+}
+
+var users = new Users();
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+    //Benutzer Speichern
+    var u = new User(socket.id);
+    user.push(u);
+    console.log(socket.id + " connected!");
 
-  sentmessages.forEach(element => {
-    socket.emit('chat message', element);
-  });
-
-  //Willkommens Nachticht bei Join und Broadcast info an alle anderen!
-  socket.emit('chat message', "Willkommen im Chat!")
-  socket.broadcast.emit('chat message', "Benutzer verbunden!");
-
-
-  //Verwalte Chat Nachrichten
-  socket.on('chat message', function(msg){
-    //Prüfe msg auf Inhalt
-    if(msg != ""){
-      //Ist Message ein Befehl?
-      if(msg.charAt(0) == '/'){
-        if(operators.indexOf(socket.id)>-1){
-          //Admin Tools
-          if(msg == "/list Admins"){
-            socket.emit('chat message', "There is " + operators.length + " Admins:");
-            operators.forEach(element => {
-              socket.emit('chat message',"  " + element);
-            });       
-          }
-          //Zeige alle Admins
-          if(msg.indexOf("/kick") == 0){
-            var n = msg.split(" ");   
-            console.log(names.find(e => e.name === n).id);
-
-            }           
-          }
-
-
-          if(msg == "/list Admins"){
-            socket.emit('chat message', "There is " + operators.length + " Admins:");
-            operators.forEach(element => {
-              socket.emit('chat message',"  " + element);
-            });       
-          }
-
-
-          if(msg == "/help"){
-            socket.emit('chat message', "Admin Tools:");      
-            socket.emit('chat message', "Benutze /list Admins um alle Admins zu zeigen!");      
-            socket.emit('chat message', "Benutze /kick (Name) um einen User zu kicken!");
-            socket.emit('chat message', "Benutze /ping (Name) um einem User das Pingen zu ermöglichen!");
-            socket.emit('chat message', "Benutze /unset Admin um dich selbst als Admin zu entfernen!");
-            socket.emit('chat message', "Benutze /rename (name) (newname) um einen Benutzer umzubenennen!")
-            socket.emit('chat message', "User Tools:");     
-            socket.emit('chat message', "Benutze /name (name) um deinen Namen zu setzen!");
-            socket.emit('chat message', "Benutze /change name um deinen Namen zu ändern!");
-          }
+    //Vor dem Connect gesendete Nachrichten nachsenden!
+    sentmessages.forEach(element => {
+      socket.emit('chat message', element);
+    });
+    
+    //Willkommens Nachticht bei Join
+    socket.emit('chat message', "Willkommen im Chat!");
+    socket.emit('chat message', "Benutze /help für Hilfe!");
+    socket.emit('chat message', "Benutze /name (name) um dir einen Namen zu geben!");
+    
+    //Verwalte Chat Nachrichten
+    socket.on('chat message', function(msg){
+        console.log("Incoming Message: "+ msg + " by " + socket.id);
+        //User hatt name!
+        if(users.UserHasName(socket.id)){    
+            if(msg[0] == '/'){
+                //Nachricht ist ein Befehl
+                //Benutzer ist Admin
+                if(user[users.GetIndexById(socket.id)].isAdmin){
+                    if(msg == "/help"){
+                        socket.emit('chat message', "Admin Tools:");      
+                        socket.emit('chat message', "Benutze /list Admins um alle Admins zu zeigen!");      
+                        socket.emit('chat message', "Benutze /kick (Name) um einen User zu kicken!");
+                        socket.emit('chat message', "Benutze /ping (Name) um einem User das Pingen zu ermöglichen!");
+                        socket.emit('chat message', "Benutze /unset Admin um dich selbst als Admin zu entfernen!");
+                        socket.emit('chat message', "Benutze /rename (name) (newname) um einen Benutzer umzubenennen!")
+                        socket.emit('chat message', "User Tools:");     
+                        socket.emit('chat message', "Benutze /name (name) um deinen Namen zu setzen!");
+                        socket.emit('chat message', "Benutze /change name um deinen Namen zu ändern!");
+                    }
+                }
+                //Normaler Benutzer
+                else{
+                    if(msg == "/help"){
+                        socket.emit('chat message', "User Tools:");     
+                        socket.emit('chat message', "Benutze /name (name) um deinen Namen zu setzen!");
+                        socket.emit('chat message', "Benutze /change name um deinen Namen zu ändern!");
+                    }
+                }
+            } 
+            //Nachricht ist eine Chatnachricht   
+            else{   
+                //Nachricht vorbereiten       
+                var message = users.GetUserNameById(socket.id) + " " + d.toLocaleTimeString()+ " : " + msg;
+                if(user[users.GetIndexById(socket.id)].isAdmin){
+                    io.emit('admin message',message);            
+                }
+                else{
+                    io.emit('chat message',message);                                  
+                }
+            }
         }
-        //Socket ist User
+        //Kein Name!
         else{
-          //Setze Operator über Chat
-          if(msg == "/set Admin"){
-            operators.push(socket.id);
-            console.log("Made " + socket.id + " Operator!");
-            socket.emit('chat message', "Du bist nun Operator!");
-            socket.emit('make admin', 1);
-          }
-          if(msg == "/help"){
-            socket.emit('chat message', "Benutze /name (name) um deinen Namen zu setzen!");
-            socket.emit('chat message', "Benutze /change name um deinen Namen zu ändern!");
-          }
+            //Nur Befehle!       
+            if(msg[0] == '/' ){
+                if(msg == "/help"){
+                    socket.emit('chat message', "Benutze /name (name) um dir einen Namen zu geben!");
+                }
+                if(msg.indexOf("/name") == 0){
+                    try {                  
+                        var n = msg.split(" ");
+                        user[users.GetIndexById(socket.id)].name = n[1];   
+                        console.log(socket.id + " set username to " + user[users.GetIndexById(socket.id)].name);
+                        socket.emit('chat message', "Du heißt nun "+ user[users.GetIndexById(socket.id)].name+" !");
+                    } catch (error) {
+                        socket.emit('chat message', "Dieser Nutzername ist nicht verfügbar!");
+                    }
+                }
+            }
+        }     
+    });
+  
+  
+    //Map Ping
+    socket.on('ping map',function(ping){
+        if(user[users.GetIndexById(socket.id)].isAdmin || user[users.GetIndexById(socket.id)].canPing){
+            io.emit('ping map',ping);
         }
-
-        //Befehle für Alle:
-
-
-        //Namen setzen
-        if(msg.indexOf("/name") == 0){
-          
-          if(!(!!names.find(e => e.id === socket.id))){
-            var n = msg.split(" ");         
-            names.push({id: socket.id ,name: n[1]});
-            socket.emit('chat message', "Du heißt nun "+n[1]);
-          }
-          //Benutzer hatt bereits namen 
-          else{
-            socket.emit('chat message', "Du hast bereits einen Namen!");
-          }
-        }
-       
-
-      }
-      else{
-        //Chatten nur erlaubt wenn Benutzer einen Username hatt!
-        if(!!names.find(e => e.id === socket.id)){
-          //Normale Chatnachricht
-          var username = names.find(e => e.id === socket.id).name;
-          var message = username + " " + d.toLocaleTimeString()+ " : " +msg;
-          console.log(message);
-          io.emit('chat message',message);
-          sentmessages.push(message);
-        }
-        else{
-          socket.emit('chat message',"Wähle einen Username mit /name (name) um zu chatten!");
-        }
-      }             
-    }
-  });
-
-
-  //Map Ping
-  socket.on('ping map',function(ping){
-    if(operators.indexOf(socket.id)>-1 || ping.indexOf(socket.id)>-1){
-      console.log(ping);
-      io.emit('ping map', ping);
-    }
-  });
-
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-    io.emit('chat message', "Benutzer getrennt");
-
-    //Operator Disconnect 
-    //Löscht Operator aus Array
-    var i = operators.splice(operators.indexOf(socket.id),1);
-    if(i==-1)
-      console.log("Operator hatt den Raum verlassen!");
-  });
-
-
+    });
+  
+    socket.on('disconnect', function(){  
+      console.log('user disconnected');
+      io.emit('chat message', "Benutzer getrennt");
+    });
 });
-
+  
 http.listen(3000, function(){
   console.log('listening on *:3000');
   console.log('Lets get shit going...');
 });
+  
